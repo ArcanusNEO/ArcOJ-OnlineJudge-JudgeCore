d4eda3aa5350999a9accf73555b8e8d56b822407
SunriseFox <sunrisefox@vampire.rip>
2019-04-11 19:16:58 +0800

    adjustment

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MOUNTFOLDER="/var/www/onlinejudge"
-MOUNTPOINT="/mnt/data"
+MOUNTFOLDER="/var/www/data"
+MOUNTPOINT="/var/www/data"
 
 # if a warning about SIGNAL occurred on build, use this:
 # dpkg -r --force-depends golang-docker-credential-helpers
@@ -9,11 +9,6 @@ MOUNTPOINT="/mnt/data"
 
 set -ex; \
   docker build -t sunrisefox/judgecore:v1.0 .; \
-  docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v $MOUNTFOLDER:$MOUNTPOINT --tmpfs /tmp sunrisefox/judgecore:v1.0; \
+  docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v $MOUNTFOLDER:$MOUNTPOINT --tmpfs /tmp:exec sunrisefox/judgecore:v1.0; \
 
 # echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-
-
-docker stop judgecore;
-docker rm judgecore;
-docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v /var/www/data:/var/www/data --tmpfs /tmp:exec sunrisefox/judgecore:v1.0; \
