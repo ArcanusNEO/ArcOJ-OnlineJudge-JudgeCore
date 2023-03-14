5debcea1e09506f293ef23838711776aa3a73990
ArcanusNEO <admin@transcliff.top>
2023-03-14 16:45:24 +0800

    Delete win bat scripts

--- build.bat
+++
@@ -1,7 +0,0 @@
-@set MOUNTFOLDER="D:\judgecore"
-@set MOUNTPOINT="/mnt/data"
-
-@docker build -t sunrisefox/judgecore:v1.0 .
-@docker run --restart always --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v %MOUNTFOLDER%:%MOUNTPOINT% --tmpfs /tmp:exec --ulimit 'stack=-1' sunrisefox/judgecore:v1.0
-
-@REM @docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v "D:\0bysj\core":/mnt/core -v "D:\0bysj\volume":/mnt/data sunrisefox/judgecore:v1.0
