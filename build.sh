f8f54e73b3f2704f3e1e5409475d0d166be12f35
ArcanusNEO <admin@transcliff.top>
2023-04-07 15:21:16 +0800

    Fix build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOUNTFOLDER="/var/www/data"
+MOUNTDIR="/var/www/data"
 MOUNTPOINT="/var/www/data"
 
 # if a warning about SIGNAL occurred on build, use this:
@@ -11,6 +11,6 @@ set -ex
 
 docker build -t arcoj/judgecore:0.2.1 .
 
-docker run --restart=always --name=judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt='apparmor=unconfined' --security-opt='seccomp=unconfined' -itd -v $MOUNTFOLDER:$MOUNTPOINT --tmpfs='/tmp:exec' --ulimit='stack=-1' arcoj/judgecore:0.2.1
+docker run --restart=always --name=judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt='apparmor=unconfined' --security-opt='seccomp=unconfined' -itd -v "${MOUNTDIR}":"${MOUNTPOINT}" --tmpfs='/tmp:exec' --ulimit='stack=-1' arcoj/judgecore:0.2.1
 
 # echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
